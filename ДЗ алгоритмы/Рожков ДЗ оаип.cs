561ac86267c10473ca4cc70bca76e0b685e1fbf7
Vovchat <115476114+Vovchat@users.noreply.github.com>
2023-09-28 20:24:18 +0300

    задания 4, 5, 6 из практикума

---
+++ ДЗ алгоритмы/Рожков ДЗ оаип.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите оценку: ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.Write("Плохо");
+        break;
+    case 2:
+        Console.Write("Неудоволетворительно");
+        break;
+    case 3:
+        Console.Write("Удоволетворительно");
+        break;
+    case 4:
+        Console.Write("Хорошо");
+        break;
+    case 5:
+        Console.Write("Отлично");
+        break;
+}
+Console.ReadKey(true);
