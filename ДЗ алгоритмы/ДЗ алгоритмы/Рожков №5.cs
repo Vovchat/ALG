add7b40fa0245f2968ac68eb00ed6351377bd6aa
Vovchat <115476114+Vovchat@users.noreply.github.com>
2023-09-28 20:34:20 +0300

    Add files via upload

---
+++ ДЗ алгоритмы/ДЗ алгоритмы/Рожков №5.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число от 1 до 12");
+int x = int.Parse(Console.ReadLine());
+if ( x== 1)
+{ Console.WriteLine("Январь"); }
+if (x == 2)
+{ Console.WriteLine("Февраль"); }
+if (x == 3)
+{ Console.WriteLine("Март"); }
+if (x == 4)
+{ Console.WriteLine("Апрель"); }
+if (x == 5)
+{ Console.WriteLine("Май"); }
+if (x == 6)
+{ Console.WriteLine("Июнь"); }
+if (x == 7)
+{ Console.WriteLine("Июль"); }
+if (x == 8)
+{ Console.WriteLine("Август"); }
+if (x == 9)
+{ Console.WriteLine("Сентябрь"); }
+if (x == 10)
+{ Console.WriteLine("Октябрь"); }
+if (x == 11)
+{ Console.WriteLine("Ноябрь"); }
+if (x == 12)
+{ Console.WriteLine("Декабрь"); }
