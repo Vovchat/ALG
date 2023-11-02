a1528f1eba3b42e6b6dae923ea26f1fe5df416cb
Vovchat <115476114+Vovchat@users.noreply.github.com>
2023-11-02 23:28:36 +0300

    Add files via upload

---
+++ Задания стр13.cs
@@ -0,0 +1,43 @@
+﻿//стр.13 зад.2
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (x * x + y * y >= 9 && x * x + y * y <= 25 && x >= 0)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+
+//стр.13 зад.3
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y <= 4 && y >= 0 && x >= -2 && x <= 4 || y >= -3 && y <= 0 && x >= -4 && x <= 5)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+
+//стр.13 зад.4
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y >= 7 - x && y >= x - 5 && y <= 5 + x && y <= -x + 13)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
