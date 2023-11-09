2f7033822c577f7f03709d463c0a938ec4a13a92
Vovchat <115476114+Vovchat@users.noreply.github.com>
2023-11-09 21:02:20 +0300

    Add files via upload

---
+++ Индивид Рожков.cs
@@ -0,0 +1,13 @@
+﻿//стр.16 зад.17 Рожков
+Console.WriteLine("Введите x");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x * x + y * y <= 25 && x >= 0 && y >= 0 || Math.Abs(x) + Math.Abs(y) <= 5 && x <= 0 && y >= 0 || Math.Abs(x) + Math.Abs(y) <= 5 && x <= 0 && y <= 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
